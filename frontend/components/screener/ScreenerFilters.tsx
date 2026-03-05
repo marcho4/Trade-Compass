b4e9f8cfe5b9ac468d1c8dd39f77046583c745f5
marcho4 <markdergilev@yandex.ru>
2026-03-05 23:14:13 +0300

    фикс зависимостей

--- frontend/components/screener/ScreenerFilters.tsx
+++ frontend/components/screener/ScreenerFilters.tsx
@@ -114,10 +114,10 @@ export const ScreenerFilters = ({
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="any">Любой</SelectItem>
-                  <SelectItem value="1">1</SelectItem>
-                  <SelectItem value="2">2</SelectItem>
-                  <SelectItem value="3">3</SelectItem>
-                  <SelectItem value="4">4</SelectItem>
+                  <SelectItem value="1">1+</SelectItem>
+                  <SelectItem value="2">2+</SelectItem>
+                  <SelectItem value="3">3+</SelectItem>
+                  <SelectItem value="4">4+</SelectItem>
                   <SelectItem value="5">5</SelectItem>
                 </SelectContent>
               </Select>
